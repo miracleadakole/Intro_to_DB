39d8a1065171c08551961488388c36b485826af8
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-10-16 14:43:59 -0700

    Updated SQL script with new changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS Customers ( customer_id INT AUTO_INCREMENT PRIMARY KE
 --Orders TABLE
 CREATE TABLE IF NOT EXISTS Orders( order_id INT AUTO_INCREMENT PRIMARY KEY, customer_id INT NOT NULL, order_date DATE NOT NULL, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id));
 
---Orders_Details TABLE
+--Order_Details TABLE
 CREATE TABLE IF NOT EXISTS Order_Details ( orderdetailid INT AUTO_INCREMENT PRIMARY KEY, order_id INT NOT NULL, book_id INT NOT NULL, quantity DOUBLE NOT NULL, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY(book_id) REFERENCES Books(book_id));
