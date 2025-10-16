02af5113bd592b08f9e9a67caa6434f57d892521
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-10-16 13:25:19 -0700

    Added alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,17 @@
+--CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+--SELECT DATABASE
+USE alx_book_store;
+
+--Authors TABLE
+CREATE TABLE IF NOT EXISTS Authors( author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARCHAR(215) NOT NULL);
+
+--Books TABLE
+CREATE TABLE IF NOT EXISTS Books (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130) NOT NULL, author_id INT NOT NULL, price DOUBLE NOT NULL, publication_date DATE, FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id));
+
+--Customers TABLE
+CREATE TABLE IF NOT EXISTS Customers ( customer_id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(215) NOT NULL, email VARCHAR(215) UNIQUE NOT NULL, address TEXT);
+
+--Orders_Details TABLE
+CREATE TABLE IF NOT EXISTS Order_Details ( orderdetailid INT AUTO_INCREMENT PRIMARY KEY, order_id INT NOT NULL, book_id INT NOT NULL, quantity DOUBLE NOT NULL, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY(book_id) REFERENCES Books(book_id));
