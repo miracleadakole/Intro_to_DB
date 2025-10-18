0d2a9741a141fc396b3310e89d05df1319729e07
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-10-18 15:16:06 -0700

    fixed changes on SQL script

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -46,6 +46,6 @@ Order_Details(
     order_id INT NOT NULL, 
     book_id INT NOT NULL, 
     quantity DOUBLE NOT NULL, 
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id), 
     FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
