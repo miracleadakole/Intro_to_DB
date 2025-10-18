f074e747a66e909e76df8cd655b14c56b33a8104
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-10-18 15:27:07 -0700

    Fix on SQL script with some new changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY, 
     customer_id INT NOT NULL, 
     order_date DATE NOT NULL, 
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
 --Order_Details TABLE
@@ -46,6 +46,6 @@ Order_Details(
     order_id INT NOT NULL, 
     book_id INT NOT NULL, 
     quantity DOUBLE NOT NULL, 
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id), 
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id), 
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
 );
